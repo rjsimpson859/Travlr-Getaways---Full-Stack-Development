48c848dcae15e4300c5b8b88ac24c1fecc4508e1
Roy Simpson <roy.jordan.simpson@gmail.com>
2023-07-22 21:45:23 -0700

    Create object schema with Mongoose, populate MongoDB

---
+++ .seedgooserc.js
@@ -0,0 +1,7 @@
+// .seedgooserc.js
+module.exports = {
+    modelBaseDirectory: 'app_server/database/models', // model directory name
+    models: ['*.js', '!db.js'], // model matcher
+    data: 'data', // data directory name
+    db: 'mongodb://localhost:27017/travlr' // db connection url
+  };
