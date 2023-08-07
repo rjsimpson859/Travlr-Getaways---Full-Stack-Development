af8afe5c2a429030c7542632305066ce7725cfc6
Roy Simpson <roy.jordan.simpson@gmail.com>
2023-08-06 18:07:09 -0700

    Create new application API part 2

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,45 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require("request");
+const apiOptions = {
+  server: "http://localhost:3000",
+};
 
+const renderTravelList = (rec, res, responseBody) => {
+  let message = null;
+  let pageTitle = process.env.npm_package_description + " Travel";
 
-/* GET travel view */
-const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', { title: pageTitle, trips});
-   };
-   module.exports = {
-    travel
-   };
+  if (!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No trips exist in database";
+    }
+  }
+  res.render("travel", {
+    title: pageTitle,
+    trips: responseBody,
+    message,
+  });
+};
+
+/* get travel list*/
+const travelList = (req, res) => {
+  const path = "/api/trips";
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: "GET",
+    json: {},
+  };
+
+  console.info(" >> travelcontroller.travelList calling" + requestOptions.url);
+  request(requestOptions, (err, { statusCode }, body) => {
+    if (err) {
+      console.error(err);
+    }
+    renderTravelList(req, res, body);
+  });
+};
+
+module.exports = {
+  travelList,
+};
