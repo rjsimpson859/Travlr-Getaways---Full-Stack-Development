fae38b04abae1bf05aca9a969d9a97e660eb6dbc
Roy Simpson <roy.jordan.simpson@gmail.com>
2023-08-13 21:46:10 -0700

    security

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
+
+const passport = require('passport');
+
+
 require('./app_api/database/db');
+
+require('./app_api/config/passport');
 
 
 var indexRouter = require('./app_server/routes/index');
@@ -28,12 +36,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Allow CORS
-app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
@@ -41,6 +53,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
+
+//catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/database/db.js
+++ app_api/database/db.js
@@ -59,3 +59,4 @@ process.on('SIGTERM', () => {
 connect();
 
 require("./models/travlr");
+require("./models/user");
